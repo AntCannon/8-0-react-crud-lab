dd22b89e4a7144853fdb1f84f8dcba2718377438
AntCannon <anthonycannonier@gmail.com>
2024-04-28 12:45:48 -0400

    add getAllShow get, display data with ShowListing

--- src/components/shows/ShowListing.js
+++ src/components/shows/ShowListing.js
@@ -1,4 +1,5 @@
 import {Link} from 'react-router-dom'
+
 import "./ShowListing.css";
 
 export default function ShowListing({ show }) {
--- src/components/shows/ShowsIndex.js
+++ src/components/shows/ShowsIndex.js
@@ -1,12 +1,29 @@
 import { Link } from "react-router-dom";
-import { useState } from "react"
+import { useState, useEffect } from "react"
+
+import { getAllShows } from "../../api/fetch.js"
 
 import ErrorMessage from "../errors/ErrorMessage";
+import ShowListing from "./ShowListing";
+
 
 import "./ShowsIndex.css";
 
 export default function ShowsIndex() {
   const [ loadingError, setLoadingError ] = useState(false);
+  const [ shows, setShows ] = useState([]);
+
+  useEffect(() => {
+    getAllShows()
+      .then((response) => {
+        setShows(response);
+        setLoadingError(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setLoadingError(true);
+      });
+  }, []);
 
   return (
     <div>
@@ -29,7 +46,9 @@ export default function ShowsIndex() {
             />
           </label>
           <section className="shows-index">
-            {/* <!-- ShowListing components --> */}
+            {shows.map((show) => {
+              return <ShowListing show={show} key={show.id} />;
+            })}
           </section>
         </section>
       )}
